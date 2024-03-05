b24c917ff081d43df7722ff7b5ee3fce04c9ec61
Daiana de Paula <daianaadepaula1@gmail.com>
2024-03-05 08:55:12 -0300

    feat: inicial project

---
+++ gabarito.py
@@ -0,0 +1,38 @@
+# Passo a passo do projeto
+# Passo 1: Entrar no sistema da empresa 
+    # https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+
+
+# pyautogui.write -> escrever um texto
+# pyautogui.press -> apertar 1 tecla
+# pyautogui.click -> clicar em algum lugar da tela
+# pyautogui.hotkey -> combinação de teclas
+
+# abrir o navegador (chrome)
+
+
+# entrar no link 
+# "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+
+
+# Passo 2: Fazer login
+# selecionar o campo de email
+
+# escrever o seu email
+# "pythonimpressionador@gmail.com"
+# passando pro próximo campo
+# clique no botao de login
+
+# Passo 3: Importar a base de produtos pra cadastrar
+
+
+# Passo 4: Cadastrar um produto
+    # clicar no campo de código
+    # pegar da tabela o valor do campo que a gente quer preencher
+    # preencher o campo
+    # passar para o proximo campo
+    # preencher o campo
+    # cadastra o produto (botao enviar)
+    # dar scroll de tudo pra cima
+    # Passo 5: Repetir o processo de cadastro até o fim
