13733faf0326892ccf2c795045f11f635ffecb36
Daiana de Paula <daianaadepaula1@gmail.com>
2024-03-10 10:40:18 -0300

    feat: form auto

--- codigo.py
+++ codigo.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
 
-pyautogui.PAUSE = 3.5
+pyautogui.PAUSE = 7.5
 
 pyautogui.press("win")
 pyautogui.write("chrome")
@@ -10,7 +10,12 @@ pyautogui.press("enter")
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 pyautogui.write(link)
 pyautogui.press("enter")
+time.sleep(12)
 
-time.sleep(8)
+pyautogui.click(x=455, y=410)
+pyautogui.write("daianaadepaula1@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("senhaAqui")
+pyautogui.click(x=671, y=572)	
+time.sleep(12)
 
-pyautogui.click()
